9c78f0b9a351ea802187399638bde960a3e479da
Kullen Wetherby <wetherbyk71@gmail.com>
2019-09-30 21:42:08 -0400

    Car models

---
+++ objects-in-array.js
@@ -0,0 +1,20 @@
+let car = [{
+    make: "Honda",
+    model: "Civic",
+    year: 2019
+
+
+}, {
+    make: "Chevy",
+    model: "Impala",
+    year: 2019
+
+
+}, {
+    make: "Ford",
+    model: "Mustang",
+    year: 2019
+
+
+}] 
+console.log(car)
