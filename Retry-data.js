f9dbdaf908387fe9a2a519775dee12d0d9886442
Kullen Wetherby <wetherbyk71@gmail.com>
2019-09-30 21:14:23 -0400

    Shows examples of data types

---
+++ Retry-data.js
@@ -0,0 +1,8 @@
+let me = {
+    firstName: 'Kullen'
+    lastName: 'Wetherby'
+    age: 22
+    birthday: '07-20-1997'
+}
+    console.log(me)
+    
---
+++ data-types.js
@@ -0,0 +1,29 @@
+var myArray = ['Info About Me'] 
+    console.log(myArray)
+var myBigArray = ['Kullen','Wetherby'] 
+    console.log(myBigArray)
+
+var birthday = new Date ('1997-07-20') 
+    console.log('Birthday', birthday)
+
+var xmas = new Date ('2018-12-25') 
+    console.log('Christmas', xmas)
+
+var myNum = 71; 
+    console.log('Favorite number', myNum)
+
+let name = "Kullen" 
+    console.log(name)
+let obj = {} 
+    console.log(obj)
+let me = {
+        name: 'Kullen'
+        age: 22
+}
+    console.log(me)
+
+
+
+
+
+
