fdaeeeb0a2f1eab0d196d41aa5c43cd838050401
Kullen Wetherby <wetherbyk71@gmail.com>
2019-10-02 20:58:11 -0400

    Filter for legal age of loto

---
+++ car-map-reduce-filter.js
@@ -0,0 +1,24 @@
+let cars = [ {
+    make: 'jeep',
+    model: 'wrangler',
+    year: 2019
+},
+    {
+    make: 'honda',
+    model: 'civic',
+    year: 2017
+}, {
+    make: 'bmw',
+    model: 'm4',
+    year: 2019
+}
+] 
+
+
+let newCars = cars.filter( (car) = {
+     return car.year = 2019 } )
+    
+
+
+
+ console.log(newCars)
